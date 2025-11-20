2c0fa579d2174972fa83a1a3fb30015ec0602e21
Killian Cassard <killian.cassard@epitech.eu>
2025-11-20 19:27:17 +0100

    step 1

---
+++ step1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 7;
+    int b = 1;
+
+    if ((a + b) == 8)
+        puts("Normal.");
+    else
+        puts("Hmm wait what ?!");
+    return b;
+}
+
